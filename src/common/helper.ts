94ef1d138382fd8dd66566a5d07631305c33a595
Duckhee Lee <shldhee@yahoo.co.kr>
2020-01-27 14:49:32 +0900

    :sparkles: Create dynamodb.ts

---
+++ src/common/helper.ts
@@ -0,0 +1,18 @@
+import HttpStatusCode from "./httpStatusCode";
+import { APIGatewayProxyResult } from "aws-lambda";
+
+const response = (
+  status: HttpStatusCode,
+  data: IResponseBody
+): APIGatewayProxyResult => {
+  return {
+    statusCode: status,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+};
+
+export { response };
